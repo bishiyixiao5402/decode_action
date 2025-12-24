897bb324061e6e2bca4ac2c2a8f5db8ba7b153f4
action <action@github.com>
2025-12-24 02:50:31 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Jun 13 2025 08:11:54 GMT+0000 (Coordinated Universal Time)
+//Wed Dec 24 2025 02:50:31 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("福田e家");
